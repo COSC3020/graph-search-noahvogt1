5ad4c1e210be623aeba5254b182fa55903adc116
noahvogt1 <96090888+noahvogt1@users.noreply.github.com>
2025-03-26 15:30:31 -0600

    Update code.js

--- code.js
+++ code.js
@@ -1,3 +1,27 @@
-function depthFirstSearch(graph, startNode, targetNode) {
-    return [];
+function depthFirstSearch(graph, startNode, targetNode, nodesVisited = []) {
+    if (startNode == targetNode) {
+        nodesVisited.push(startNode);
+        return nodesVisited;
+    }
+
+    for (let i = 0; i < graph.length; i++) {
+        if (graph[startNode][i] == 1 && startNode != i) {
+
+            nodesVisited.push(startNode);
+            let len = nodesVisited.length;
+
+            if (!nodesVisited.includes(i)) {
+                nodesVisited = depthFirstSearch(graph, i, targetNode, nodesVisited);
+            }
+
+            let len2 = nodesVisited.length;
+
+            if (len == len2) {
+                nodesVisited.pop();
+            } else {
+                return nodesVisited;
+            }
+        }
+    }
+    return nodesVisited;
 }
